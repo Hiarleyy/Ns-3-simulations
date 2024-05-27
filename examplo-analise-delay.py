f13ff0179576259001607a635d2562e9b7406e9b
Robert <151944337+devr0bert@users.noreply.github.com>
2024-05-27 00:41:42 -0300

    Update and rename examplo-analise.py to examplo-analise-delay.py

---
+++ examplo-analise-delay.py
@@ -0,0 +1,39 @@
+#%%
+from cProfile import label
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+# %%
+df = pd.read_csv('DlRlcStats.csv',usecols=['% start', 'RNTI', 'delay'	] , sep=';')
+type(df)
+#%%
+df['delay'].unique()
+#%%
+df1 = df[df['RNTI'] == 1]
+df2 = df[df['RNTI'] ==2]
+df3 = df[df['RNTI']==3]
+# %%
+# o tempo é o mesmo para todos
+tempo = df1['% start']
+# %%
+delay1 = df1['delay']
+delay1 = np.where( delay1 == 0, np.nan, delay1)
+delay2 = df2['delay']
+delay2 = np.where( delay2 == 0, np.nan, delay2)
+delay3 = df3['delay']
+delay3 = np.where( delay3 == 0, np.nan, delay3)
+pd.DataFrame(delay1)
+#%%
+
+plt.plot(tempo, delay1, label=)
+plt.plot(tempo, delay2)
+plt.plot(tempo, delay3)
+plt.title('Delay rate')
+plt.xlabel('Tempo(s)')
+plt.ylabel('Delay(ms)')
+
+
+
+plt.show
+# %%
+
