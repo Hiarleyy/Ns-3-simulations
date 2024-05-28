9fdb154c4397f28664d215d47b9c71f1d309cfdc
Robert <151944337+r0bertds@users.noreply.github.com>
2024-05-28 08:38:04 -0300

    Add files via upload

---
+++ static-user/200M/delay-200m.py
@@ -0,0 +1,32 @@
+#%%
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+#%%
+data = pd.read_csv('DlRlcStats.csv', sep=';', 
+                   usecols=['DlRlcStats% start', 'delay']
+                   )
+df = pd.DataFrame(data)
+df
+# %%
+tempo = df["DlRlcStats% start"]
+tempo
+#%%
+delay = df['delay']
+delay.value_counts()
+#%%
+delay = delay.unique()
+delay
+# %%
+tempo = np.linspace(0, 60)
+delay1 = np.full_like(tempo, delay[0])
+#delay2 = np.full_like(tempo, delay[1])
+
+#%%
+plt.plot(tempo, delay1)
+plt.title('Delay rate (200m)')
+plt.xlabel('Tempo(s)')
+plt.ylabel('Delay(ms)')
+plt.show
+
+# %%
