d578c803e6d99fc6d9b62c05f27a2d689123c2ad
Robert <151944337+r0bertds@users.noreply.github.com>
2024-05-28 08:36:20 -0300

    Add files via upload

---
+++ static-user/50m/delay-50m.py
@@ -0,0 +1,33 @@
+#%%
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+#%%
+data = pd.read_csv('DlRlcStats-100Ghz.csv', sep=';', 
+                   usecols=['% start', 'delay']
+                   )
+df = pd.DataFrame(data)
+df
+# %%
+tempo = df["% start"]
+tempo
+#%%
+delay = df['delay']
+delay.value_counts()
+#%%
+delay = delay.unique()
+delay
+# %%
+tempo = np.linspace(0, 100)
+delay1 = np.full_like(tempo, delay[0])
+#delay2 = np.full_like(tempo, delay[1])
+
+#%%
+plt.plot(tempo, delay1)
+#plt.plot(tempo, delay2)
+plt.title('Delay rate (50m)')
+plt.xlabel('Tempo(s)')
+plt.ylabel('Delay(ms)')
+plt.show
+
+# %%
