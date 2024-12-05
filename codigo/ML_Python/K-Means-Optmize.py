4ffeeaa2267875beed8b76dbfb16be01ee921c25
Marcos Hiarley <marcoshiarley.silva@gmail.com>
2024-12-05 10:48:09 -0300

    Enhance K-Means optimization script to include user IDs and save user positions to CSV; improve antenna visualization

--- codigo/ML_Python/K-Means-Optmize.py
+++ codigo/ML_Python/K-Means-Optmize.py
@@ -10,10 +10,16 @@ n_users = 100
 x_users = np.random.uniform(0, 100, n_users)  # Coordenada X dos usuários
 y_users = np.random.uniform(0, 100, n_users)  # Coordenada Y dos usuários
 user_locations = np.array(list(zip(x_users, y_users)))
+user_ids = [f'Ue{i}' for i in range(n_users)]
+user_positions_df = pd.DataFrame(user_locations, columns=['Posição X', 'Posição Y'])
+user_positions_df['ID'] = user_ids
+user_positions_df = user_positions_df[['ID', 'Posição X', 'Posição Y']]
+user_positions_df.to_csv('user_positions.csv', index=False)
+
+#definindo as posições iniciais das antenas
 antenna_positions_before = np.array([[10, 10], [10, 90], [90, 10], [90, 90]])
 # Número de antenas (clusters desejados)
 n_antennas = 4
-
 # Aplicando k-means para encontrar os clusters
 kmeans = KMeans(n_clusters=n_antennas, random_state=42)
 kmeans.fit(user_locations)
@@ -25,19 +31,14 @@ labels = kmeans.labels_
 #%%
 plt.figure(figsize=(10, 8))
 plt.scatter(x_users, y_users, c=labels, cmap='viridis', s=50, label="Usuários")
-plt.scatter(antenna_positions_before[:, 0], antenna_positions_after[:, 1], c='red', s=200, marker='o', label="Antenas")
-plt.title("Disposição de antenas manual", fontsize=16)
-plt.xlabel("X", fontsize=12)
-plt.ylabel("Y", fontsize=12)
-plt.legend(fontsize=12)
-plt.grid(True)
-plt.show()
 # Visualizando os resultados
 plt.figure(figsize=(10, 8))
 # Plotando os usuários
 plt.scatter(x_users, y_users, c=labels, cmap='viridis', s=50, label="Usuários")
 # Plotando as antenas
-plt.scatter(antenna_positions_after[:, 0], antenna_positions_after[:, 1], c='red', s=200, marker='X', label="Antenas")
+plt.scatter(antenna_positions_before[:, 0], antenna_positions_after[:, 1],edgecolors='black',c='blue', s=200, marker='X', label="Antenas Iniciais")
+plt.scatter(antenna_positions_after[:, 0], antenna_positions_after[:, 1],edgecolors='black', c='red', s=200, marker='X', label="Antenas Otimizadas")
+
 # Melhorando o visual
 plt.title("Otimização de Antenas com K-Means", fontsize=16)
 plt.xlabel("X", fontsize=12)
