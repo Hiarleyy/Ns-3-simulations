0f7ee53adf32e34e87b173f29969064dffe1fbf5
Marcos Hiarley <marcoshiarley.silva@gmail.com>
2025-01-06 19:28:10 -0300

    Add new script for SINR analysis and visualization; update K-Means optimization for user positioning

---
+++ 5g_lena-Scripts/aa.py
@@ -0,0 +1,84 @@
+#%%
+import pandas as pd 
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+
+#%%
+df = pd.read_csv('datasets\DlCtrlSinr.txt', sep='\t')
+
+df
+# %%
+# Função para filtrar DataFrame por Cellid
+def filter_by_CellId(df, CellId_values):
+    return {CellId: df[df['CellId'] == CellId] for CellId in CellId_values}
+
+CellId_values = [2,4,6,8]  # Apenas os valores de CellId 2 e 4
+filtered_dfs = filter_by_CellId(df, CellId_values)
+
+
+plt.figure(figsize=(12, 8))
+
+colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k']
+markers = ['o', 's', 'D', '^', 'v', '<', '>']
+linestyles = ['-', '--', '-.', ':', '-', '--', '-.']
+for i, CellId in enumerate(CellId_values):
+    user_df = filtered_dfs[CellId]
+    plt.plot(user_df['Time'], user_df['SINR(dB)'], label=f'CellId {CellId}', color=colors[i], marker=markers[i], linestyle=linestyles[i], markersize=1, linewidth=1.5, alpha=0.7)
+
+plt.title('SINR over Time for CellIds 2 and 4', fontsize=16)
+plt.xlabel('Time (sec)', fontsize=14)
+plt.ylabel('SINR (dB)', fontsize=14)
+plt.legend(title='CellIds', fontsize=12, title_fontsize='13')
+plt.grid(True, which='both', linestyle='--', linewidth=0.5)
+plt.minorticks_on()
+plt.tight_layout()
+plt.show()
+
+
+#%%
+# Clean plot
+plt.figure(figsize=(12, 8))
+
+colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k']
+markers = ['o', 's', 'D', '^', 'v', '<', '>']
+linestyles = ['-', '--', '-.', ':', '-', '--', '-.']
+for i, CellId in enumerate(CellId_values):
+    user_df = filtered_dfs[CellId]
+    plt.plot(user_df['Time'], user_df['SINR(dB)'], label=f'CellId {CellId}', color=colors[i], marker=markers[i], linestyle=linestyles[i], markersize=5, linewidth=1.5)
+
+plt.title('Clean SINR over Time for CellIds 2 and 4', fontsize=16)
+plt.xlabel('Time (sec)', fontsize=14)
+plt.ylabel('SINR (dB)', fontsize=14)
+plt.legend(title='CellIds', fontsize=12, title_fontsize='13')
+plt.grid(True, which='both', linestyle='--', linewidth=0.5)
+plt.minorticks_on()
+plt.tight_layout()
+plt.show()
+
+# Heatmap plot using seaborn
+#%%
+fig, axs = plt.subplots(len(CellId_values), 1, figsize=(12, 8), sharex=True)
+
+for i, CellId in enumerate(CellId_values):
+    user_df = filtered_dfs[CellId]
+    
+    # Remover duplicatas agregando pela média
+    user_df = user_df.groupby(['Time', 'CellId'], as_index=False).mean()
+    
+    pivot_table = user_df.pivot(index="Time", columns="CellId", values="SINR(dB)")
+    sns.heatmap(pivot_table, ax=axs[i], cmap="YlGnBu", cbar=True)
+    
+    axs[i].set_title(f'SINR Heatmap for CellId {CellId}', fontsize=14)
+    axs[i].set_ylabel('Time (sec)', fontsize=12)
+
+axs[-1].set_xlabel('CellId', fontsize=12)
+
+plt.tight_layout()
+plt.show()
+
+
+# %%
+grouped_df = df.groupby('CellId')['SINR(dB)'].describe()
+grouped_df
+# %%
--- codigo/ML_Python/K-Means-Optmize.py
+++ codigo/ML_Python/K-Means-Optmize.py
@@ -5,10 +5,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 #%%
 # Gerando dados simulados para a localização de usuários
-np.random.seed(11)
-n_users = 50
-x_users = np.random.uniform(0, 100, n_users)  # Coordenada X dos usuários
-y_users = np.random.uniform(0, 100, n_users)  # Coordenada Y dos usuários
+n_users = 20
+x_users = np.random.uniform(0, 80, n_users)  # Coordenada X dos usuários
+y_users = np.random.uniform(0, 80, n_users)  # Coordenada Y dos usuários
 user_locations = np.array(list(zip(x_users, y_users)))
 user_ids = [f'Ue{i}' for i in range(n_users)]
 user_positions_df = pd.DataFrame(user_locations, columns=['Posição X', 'Posição Y'])
@@ -18,7 +17,7 @@ user_positions_df.to_csv('user_positions.csv', index=False)
 
 #definindo as posições iniciais das antenas
 antenna_positions_before = np.array([[10, 10], [10, 90], [90, 10], [90, 90]])
-# Número de antenas (clusters desejados)
+# Número de ante    nas (clusters desejados)
 n_antennas = 4
 # Aplicando k-means para encontrar os clusters
 kmeans = KMeans(n_clusters=n_antennas, random_state=42)
@@ -38,13 +37,28 @@ for i, pos in enumerate(antenna_positions_after):
 #%%
 plt.figure(figsize=(10, 8))
 plt.scatter(x_users, y_users, c=labels, cmap='viridis', s=50, label="Usuários")
+
+# Adicionando o título para cada usuário
+for i, (x, y) in enumerate(zip(x_users, y_users)):
+    plt.text(x, y, f'UE {i+1}', fontsize=9, ha='right')
+
 # Visualizando os resultados
 plt.figure(figsize=(10, 8))
 # Plotando os usuários
 plt.scatter(x_users, y_users, c=labels, cmap='viridis', s=50, label="Usuários")
-# Plotando as antenas
-plt.scatter(antenna_positions_before[:, 0], antenna_positions_after[:, 1],edgecolors='black',c='blue', s=200, marker='X', label="Antenas Iniciais")
-plt.scatter(antenna_positions_after[:, 0], antenna_positions_after[:, 1],edgecolors='black', c='red', s=200, marker='X', label="Antenas Otimizadas")
+# Adicionando o título para cada usuário
+for i, (x, y) in enumerate(zip(x_users, y_users)):
+    plt.text(x, y, f'UE {i+1}', fontsize=9, ha='right')
+# Plotando as antenas iniciais
+plt.scatter(antenna_positions_before[:, 0], antenna_positions_before[:, 1], edgecolors='black', c='blue', s=200, marker='o', label="Antenas Iniciais")
+# Adicionando o título para cada antena inicial
+for i, (x, y) in enumerate(antenna_positions_before):
+    plt.text(x, y, f'ENB {i+1}', fontsize=9, ha='right')
+# Plotando as antenas otimizadas
+plt.scatter(antenna_positions_after[:, 0], antenna_positions_after[:, 1], edgecolors='black', c='red', s=200, marker='o', label="Antenas Otimizadas")
+# Adicionando o título para cada antena otimizada
+for i, (x, y) in enumerate(antenna_positions_after):
+    plt.text(x, y, f'ENB {i+1}', fontsize=9, ha='right')
 
 # Melhorando o visual
 plt.title("Otimização de Antenas com K-Means", fontsize=16)
